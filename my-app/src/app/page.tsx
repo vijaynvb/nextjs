fe26c17815dc6c2f72cedf7f1681b397823ad49d
vijay <vijay.nvb@gmail.com>
2025-05-19 14:24:08 +0530

    router-damo

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,3 +6,5 @@ export default function Home() {
 }
 
 // 1. figma wire frame -> extract the design -> html code or specific to framework nextjs -> tailwindcss 
+// scheme://ip:port/pathtoresource/pathvariable?querykey=queryvalue&querykey2=queryvalue2
+// http://localhost:3000/blog/[1]?name=John&age=30
